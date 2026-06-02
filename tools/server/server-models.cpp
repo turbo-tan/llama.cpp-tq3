439dbd3289e192677551b0ae672a31e8c15c4477
charpdev <charpdev@users.noreply.github.com>
2026-06-02 10:11:57 +0100

    fix: ui_mcp_proxy -> webui_mcp_proxy in server-models.cpp

--- tools/server/server-models.cpp
+++ tools/server/server-models.cpp
@@ -955,7 +955,7 @@ void server_models_routes::init_routes() {
                 }},
                 {"webui_settings", webui_settings},
                 {"build_info",     std::string(llama_build_info())},
-                {"cors_proxy_enabled", params.ui_mcp_proxy || params.webui_mcp_proxy},
+                {"cors_proxy_enabled", params.webui_mcp_proxy},
             });
             return res;
         }
