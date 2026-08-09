1b4dbb912741b7c837ffcea2d802db55a26c1464
Aaron Teo <aaron.teo1@ibm.com>
2026-08-09 18:15:28 +0800

    ci: rm `GGML_HIP_ROCWMMA_FATTN` (#26760)
    Signed-off-by: Aaron Teo <aaron.teo1@ibm.com>

--- ci/run.sh
+++ ci/run.sh
@@ -90,6 +90,7 @@ fi
 
 if [ ! -z ${GG_BUILD_ROCM} ]; then
     CMAKE_EXTRA="${CMAKE_EXTRA} -DGGML_HIP=ON"
+    CMAKE_EXTRA="${CMAKE_EXTRA} -DCMAKE_HIP_COMPILER=$(hipconfig -l)/clang -DGGML_HIP=ON"
     if [ -z ${GG_BUILD_AMDGPU_TARGETS} ]; then
         echo "Missing GG_BUILD_AMDGPU_TARGETS, please set it to your GPU architecture (e.g. gfx90a, gfx1100, etc.)"
         exit 1
