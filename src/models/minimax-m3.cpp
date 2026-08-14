f8dd03fa03f5d502ae1e4de5092430a11f1b634a
Xuan-Son Nguyen <son@huggingface.co>
2026-08-14 23:34:56 +0200

    mtmd, common: various fixes (#27071)
    * apply fixes
    
    * cont
    
    * revert gguf fix

--- src/models/minimax-m3.cpp
+++ src/models/minimax-m3.cpp
@@ -25,6 +25,8 @@ void llama_model_minimax_m3::load_arch_hparams(llama_model_loader & ml) {
     msa_p = { (int) hparams.indexer_block_size, (int) hparams.indexer_top_k, (int) hparams.indexer_local_blocks };
     hparams.indexer_kv = true;
 
+    GGML_ASSERT(hparams.indexer_block_size > 0); // avoid div by zero
+
     switch (hparams.n_layer()) {
         case 60: type = LLM_TYPE_428B_A23B; break;
         default: type = LLM_TYPE_UNKNOWN;
